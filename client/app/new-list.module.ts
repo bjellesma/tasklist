23b9a383fed46d981a31e50e9211ea79e8e90927
bjellesma <wjellesma@gmail.com>
2017-03-18 22:53:45 +0000

    Adding second module

---
+++ client/app/new-list.module.ts
@@ -0,0 +1,12 @@
+//all of the ts that we're loading
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms'; //needed to add forms in angular2
+import { NewListComponent} from './app.component';
+@NgModule({
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
+  declarations: [NewListComponent],
+  bootstrap: [NewListComponent]
+})
+export class AppModule { }
