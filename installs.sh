f255e9d3e02ae129a0cc9d2602fa02cbbefd7b13
bjellesma <william.jellesma@gmail.com>
2017-11-08 13:22:01 -0500

    dos2unix should be used when make the installs shell script because of different encoding in windows vs linux

--- installs.sh
+++ installs.sh
@@ -1,9 +1,7 @@
-sudo npm cache clean
+sudo npm install
 sudo npm install bower -g
 sudo npm install nodemon -g
-sudo npm install
-sudo bower install
+bower install
 cd client
-sudo npm cache clean
 sudo npm install
 echo "Installation is complete. If an error occurs when starting the server, you may need to run 'sudo npm install' seperately"
